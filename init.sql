cb5e8ddad9b028a3394894c8bfd86dbeeb66ca29
jaurel-fosset <166848398+jaurel-fosset@users.noreply.github.com>
2024-06-13 21:04:21 +0200

    Adds dictionary of word and corrects some mistakes in init.sql

--- init.sql
+++ init.sql
@@ -13,7 +13,7 @@ CREATE TABLE `Colors`
 CREATE TABLE `Cards`
 (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `word` VARCHAR(30),
+    `word` VARCHAR(30) NOT NULL,
     `idColor` INT NOT NULL,
     PRIMARY KEY(`id`)
 );
@@ -60,7 +60,7 @@ ON DELETE CASCADE;
 CREATE TABLE `Players`
 (
     `id` INT NOT NULL AUTO_INCREMENT,
-    `returnCode` CHAR(10) NOT NULL,
+    `returnCode` CHAR(11) NOT NULL,
     `pseudo` VARCHAR(30) NOT NULL,
     PRIMARY KEY(`id`)
 );
@@ -96,3 +96,59 @@ FOREIGN KEY (`idGrid`)
 REFERENCES `Grids` (`id`)
 ON UPDATE CASCADE
 ON DELETE NO ACTION;
+
+INSERT INTO `Colors` (name)
+VALUES ('blue'), ('grey'), ('black') ;
+
+INSERT INTO `Cards` (word, idColor)
+VALUES
+    ('mix', 1),
+    ('field', 1),
+    ('activist', 1),
+    ('weapon', 1),
+    ('tank', 1),
+    ('battle', 1),
+    ('adequate', 1),
+    ('carrier', 1),
+    ('gear', 1),
+    ('inquiry', 1),
+    ('conventional', 1),
+    ('personality', 1),
+    ('line', 1),
+    ('adventure', 1),
+    ('impact', 1),
+    ('regulate', 1),
+    ('live', 2),
+    ('treat', 2),
+    ('anymore', 2),
+    ('frequent', 2),
+    ('poetry', 2),
+    ('running', 2),
+    ('device', 2),
+    ('agree', 2),
+    ('electric', 2),
+    ('lady', 2),
+    ('reference', 2),
+    ('pet', 2),
+    ('reading', 2),
+    ('partner', 2),
+    ('clear', 2),
+    ('would', 2),
+    ('golf', 2),
+    ('winner', 2),
+    ('net', 2),
+    ('gift', 2),
+    ('confront', 2),
+    ('wealthy', 2),
+    ('policy', 2),
+    ('deputy', 2),
+    ('serve', 2),
+    ('dog', 2),
+    ('virtue', 2),
+    ('target', 2),
+    ('recogniz', 2),
+    ('blue', 2),
+    ('ability', 3),
+    ('muscle', 3),
+    ('egg', 3),
+    ('package', 3) ;
